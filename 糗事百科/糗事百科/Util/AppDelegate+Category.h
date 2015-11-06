d11eda680da3db0c563596b5cedf08814de213ba
Hanten <hantianit@outlook.com>
2015-11-06 20:29:32 +0800

    引入WMPageController框架

--- 糗事百科/糗事百科/Util/AppDelegate+Category.h
+++ 糗事百科/糗事百科/Util/AppDelegate+Category.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  AppDelegate+Category.h
 //  糗事百科
 //
 //  Created by apple-jd35 on 15/11/6.
 //  Copyright © 2015年 hzq. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
-@interface ViewController : UIViewController
-
+@interface AppDelegate (Category)
 
 @end
-
